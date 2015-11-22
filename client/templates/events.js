5f817f3f9b834705b7f52629c6711695590eb34e
Jacek Krecioch <jacek.krecioch@gmail.com>
2015-11-22 10:39:28 +0100

    Removed the Insecure package

--- client/templates/events.js
+++ client/templates/events.js
@@ -13,13 +13,7 @@ Template.loggedin.events({
     var f_name = event.target.field_name.value;
     var f_value = event.target.field_value.value;
 
-    // Insert a task into the collection
-    Fields.insert({
-      userId: Meteor.userId(),
-      field_name: f_name,
-      field_value: f_value,
-      createdAt: new Date() // current time
-    });
+    Meteor.call("addField", f_name, f_value)
 
     // Clear form
     event.target.field_name.value = "";
@@ -32,7 +26,7 @@ Template.loggedin.events({
 
 Template.user_field.events({
   "click .remove-info": function () {
-    Fields.remove(this._id);
+    Meteor.call("deleteField", this._id);
   }
 });
 
--- tellme.js
+++ tellme.js
@@ -1,1 +1,17 @@
 Fields = new Mongo.Collection("fields");
+Meteor.methods({
+  addField: function (f_name, f_value) {
+
+    // Insert a task into the collection
+    Fields.insert({
+      userId: Meteor.userId(),
+      field_name: f_name,
+      field_value: f_value,
+      createdAt: new Date() // current time
+    });
+
+  },
+  deleteField: function (field_id) {
+    Fields.remove(field_id);
+  }
+});
